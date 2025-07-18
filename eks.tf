2f3c76ead6416b818456a2641f5368f4ff731698
karthikpdm <karthikbm.pdm19@rvce.edu.in>
2025-07-18 10:44:35 +0530

    changed

--- eks.tf
+++ eks.tf
@@ -9,10 +9,10 @@ resource "aws_eks_cluster" "main" {
     subnet_ids              = [data.aws_subnet.private_az1.id, data.aws_subnet.private_az2.id]
     security_group_ids      = [aws_security_group.eks_cluster.id]
     endpoint_private_access = true   # API server accessible from VPC
-    endpoint_public_access  = true  # Changed to true for OIDC access
-    public_access_cidrs     = ["0.0.0.0/0"]
-    # endpoint_public_access  = false  # API server NOT accessible from internet
-    # public_access_cidrs     = []     # Empty since public access is disabled
+    # endpoint_public_access  = true  # Changed to true for OIDC access
+    # public_access_cidrs     = ["0.0.0.0/0"]
+    endpoint_public_access  = false  # API server NOT accessible from internet
+    public_access_cidrs     = []     # Empty since public access is disabled
   }
 
   # Enable EKS Cluster logging - All log types for better observability
@@ -103,23 +103,23 @@ resource "aws_eks_addon" "kube_proxy" {
 # }
 
 # OIDC Identity Provider (only after cluster is fully ready)
-# data "tls_certificate" "eks_cluster" {
-#   url = aws_eks_cluster.main.identity[0].oidc[0].issuer
+data "tls_certificate" "eks_cluster" {
+  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
 
-#   depends_on = [aws_eks_cluster.main]
-# }
+  depends_on = [aws_eks_cluster.main]
+}
 
-# resource "aws_iam_openid_connect_provider" "eks_cluster" {
-#   client_id_list  = ["sts.amazonaws.com"]
-#   thumbprint_list = [data.tls_certificate.eks_cluster.certificates[0].sha1_fingerprint]
-#   url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
+resource "aws_iam_openid_connect_provider" "eks_cluster" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks_cluster.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
 
-#   depends_on = [aws_eks_cluster.main]
+  depends_on = [aws_eks_cluster.main]
 
-#   tags = {
-#     Name = "bsp-eks-cluster1-oidc-provider"
-#   }
-# }
+  tags = {
+    Name = "bsp-eks-cluster1-oidc-provider"
+  }
+}
 
 
 
