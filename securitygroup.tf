9955186e1ae554d9571f6a14d73f968df6e6eaaa
karthikpdm <karthikbm.pdm19@rvce.edu.in>
2025-07-19 21:49:26 +0530

    changed

--- securitygroup.tf
+++ securitygroup.tf
@@ -2,7 +2,7 @@
 
 # EKS Cluster Security Group
 resource "aws_security_group" "eks_cluster" {
-  name        = "bsp-eks-clusters-sg"
+  name        = "bsp-eks-clusters1-sg"
   description = "Security group for EKS cluster control plane"
   vpc_id      = data.aws_vpc.existing.id
 
