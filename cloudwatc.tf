fb8b85ef4af8117f4454980b736e97d2d716f630
karthikpdm <karthikbm.pdm19@rvce.edu.in>
2025-07-22 00:27:21 +0530

    changed

--- cloudwatc.tf
+++ cloudwatc.tf
@@ -413,6 +413,19 @@
 
 # Note: data sources for account ID and region are already in main.tf
 
+# fluent-bit-logging.tf
+
+# Note: data sources for account ID and region are already in main.tf
+
+# fluent-bit-logging.tf
+
+# Note: data sources for account ID and region are already in main.tf
+
+# IAM Policy for Fluent Bit CloudWatch Logs
+# fluent-bit-logging.tf
+
+# Note: data sources for account ID and region are already in main.tf
+
 # IAM Policy for Fluent Bit CloudWatch Logs
 resource "aws_iam_policy" "fluent_bit_cloudwatch_policy" {
   name        = "FluentBitCloudWatchLogsPolicy"
@@ -577,9 +590,9 @@ resource "kubernetes_config_map" "fluent_bit_config" {
           Name                cloudwatch_logs
           Match               kube.*
           region              ${data.aws_region.current.name}
-          log_group_name      /aws/eks/${aws_eks_cluster.main.name}/$${kubernetes['namespace_name']}
+          log_group_name      /aws/eks/${aws_eks_cluster.main.name}/${kubernetes["namespace_name"]}
           log_stream_prefix   pod-
-          log_stream_name     $${kubernetes['pod_name']}
+          log_stream_name     ${kubernetes["pod_name"]}
           auto_create_group   true
           log_retention_days  7
           log_key             log
