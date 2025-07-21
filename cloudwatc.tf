43344531c9e0b32f205eb6e1c949f9859356427c
karthikpdm <karthikbm.pdm19@rvce.edu.in>
2025-07-22 00:30:31 +0530

    changed

--- cloudwatc.tf
+++ cloudwatc.tf
@@ -590,9 +590,9 @@ resource "kubernetes_config_map" "fluent_bit_config" {
           Name                cloudwatch_logs
           Match               kube.*
           region              ${data.aws_region.current.name}
-          log_group_name      /aws/eks/${aws_eks_cluster.main.name}/${kubernetes["namespace_name"]}
+          log_group_name      /aws/eks/${aws_eks_cluster.main.name}/$${kubernetes['namespace_name']}
           log_stream_prefix   pod-
-          log_stream_name     ${kubernetes["pod_name"]}
+          log_stream_name     $${kubernetes['pod_name']}
           auto_create_group   true
           log_retention_days  7
           log_key             log
